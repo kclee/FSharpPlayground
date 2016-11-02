6e4a2d9536a8d5de16095a383422e8ac447fab8f
KC Lee <kckckckckckc@gmail.com>
2016-11-02 00:33:52 -0500

    FSExpert Files

--- FSharpPlayground/DEV207_M1/AssemblyInfo.fs
+++ FSharpPlayground/DEV207_M1/AssemblyInfo.fs
@@ -1,4 +1,4 @@
-﻿namespace ConsoleApplication1.AssemblyInfo
+﻿namespace DEV207_M1.AssemblyInfo
 
 open System.Reflection
 open System.Runtime.CompilerServices
@@ -7,11 +7,11 @@ open System.Runtime.InteropServices
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[<assembly: AssemblyTitle("ConsoleApplication1")>]
+[<assembly: AssemblyTitle("DEV207_M1")>]
 [<assembly: AssemblyDescription("")>]
 [<assembly: AssemblyConfiguration("")>]
 [<assembly: AssemblyCompany("")>]
-[<assembly: AssemblyProduct("ConsoleApplication1")>]
+[<assembly: AssemblyProduct("DEV207_M1")>]
 [<assembly: AssemblyCopyright("Copyright ©  2016")>]
 [<assembly: AssemblyTrademark("")>]
 [<assembly: AssemblyCulture("")>]
@@ -22,7 +22,7 @@ open System.Runtime.InteropServices
 [<assembly: ComVisible(false)>]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[<assembly: Guid("7d595f25-5558-4328-87f7-3d460d66d03c")>]
+[<assembly: Guid("81b687ef-259f-4ce0-b863-22aac202c3d0")>]
 
 // Version information for an assembly consists of the following four values:
 // 
