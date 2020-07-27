e0f7bddffe5387606c0b6c076fab150ba8def5d1
KC Lee <kckckckckckc@gmail.com>
2020-07-27 15:29:44 -0500

    Add another prog for book reading. (Get programming F#).

--- FSharpPlayground/FSExpert.Practice/Program.fs
+++ FSharpPlayground/FSExpert.Practice/Program.fs
@@ -1,7 +1,14 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
+open System
+    
+let generateStamp =
+    let mutable count = 0
+    count <- count + 1
+    count
 
 [<EntryPoint>]
 let main argv = 
     printfn "%A" argv
+    let pause = Console.ReadLine()
     0 // return an integer exit code
---
+++ FSharpPlayground/GetProgrammingBook/Program.fs
@@ -0,0 +1,28 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+// M1 T1
+let deg2rad (deg : float) = deg * Math.PI /  180.0
+let rad2deg (rad : float) = rad * 180.0 / Math.PI
+
+let testa = deg2rad 67.0
+let testb = rad2deg 67.0
+
+// M1 T2
+let hexarea t : float = (3.0 * Math.Sqrt(3.0) / 2.0) * Math.Pow(t,2.0)
+
+
+[<EntryPoint>]
+let main argv =
+    
+    // M1 T2
+    //printfn "%A" argv    
+    //let value = Console.ReadLine()
+    //let f = float value
+    //let calc = hexarea f
+    //printf "%f" calc
+    //printfn "%f" testa
+
+    let pause = Console.ReadLine()
+    0
