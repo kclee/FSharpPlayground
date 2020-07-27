7218103a9110b56337ee1f97dbc379ec6340dcc8
KC Lee <kckckckckckc@gmail.com>
2020-07-27 18:10:55 -0500

    .Net core F# to .Net Framework F# (due to Win Form reference).

--- FSharpPlayground/GetProgrammingBook/Program.fs
+++ FSharpPlayground/GetProgrammingBook/Program.fs
@@ -1,28 +1,13 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿open System
+open System.Net
+open System.Windows.Forms
 
-open System
-
-// M1 T1
-let deg2rad (deg : float) = deg * Math.PI /  180.0
-let rad2deg (rad : float) = rad * 180.0 / Math.PI
-
-let testa = deg2rad 67.0
-let testb = rad2deg 67.0
-
-// M1 T2
-let hexarea t : float = (3.0 * Math.Sqrt(3.0) / 2.0) * Math.Pow(t,2.0)
-
-
-[<EntryPoint>]
-let main argv =
-    
-    // M1 T2
-    //printfn "%A" argv    
-    //let value = Console.ReadLine()
-    //let f = float value
-    //let calc = hexarea f
-    //printf "%f" calc
-    //printfn "%f" testa
-
-    let pause = Console.ReadLine()
-    0
+let webClient = new WebClient()
+let fsharpOrg = webClient.DownloadString(Uri "http://fsharp.org")
+let browser =
+    new WebBrowser(ScriptErrorsSuppressed = true,
+                   Dock = DockStyle.Fill,
+                   DocumentText = fsharpOrg)
+let form = new Form(Text = "Hello from F#!")
+form.Controls.Add browser
+form.Show()
