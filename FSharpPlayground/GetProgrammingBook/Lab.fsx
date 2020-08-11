6281d6535cb7cbb42a16b3d2d76fca5f8a8d9b92
KC Lee <kckckckckckc@gmail.com>
2020-08-10 23:39:01 -0500

    Ch17 practice (operations on seq, map, set).

---
+++ FSharpPlayground/GetProgrammingBook/Lab.fsx
@@ -0,0 +1,16 @@
+﻿///
+/// Ch17 // run in F# interactive
+///
+
+let folderNameToDaysCreation = 
+    System.IO.Directory.EnumerateDirectories("C:\\") 
+    |> Seq.map (fun x -> new System.IO.DirectoryInfo(x))
+    |> Seq.map (fun x -> (x.Name, x.CreationTimeUtc))
+    |> Map.ofSeq
+    |> Map.map (fun name creationUtc -> (System.DateTime.UtcNow - creationUtc).Days)
+
+let folderNameToFileExtensions = 
+    System.IO.Directory.EnumerateFiles("C:\\") 
+    |> Seq.map (fun x -> new System.IO.FileInfo(x))
+    |> Seq.map (fun x -> x.Extension)
+    |> Set.ofSeq
